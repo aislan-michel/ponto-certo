0019d1cddc77059660f5a91e64e96006c105f40e
Aislan Michel Moreira Freitas <aislan.michel92@gmail.com>
2022-12-06 21:20:12 -0300

    init project

---
+++ src/PontoCerto.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PontoCerto.WebApplication.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
